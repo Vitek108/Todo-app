cad83c94e27c9a0fb131e8697c86dc5a362030d8
Vít Kuntoš <v.kuntos@gmail.com>
2021-12-17 15:18:21 +0100

    update view, delete view, bootstrap

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from todo_app.views import todo_list, todo_detail, todo_create
+from todo_app.views import todo_list, todo_detail, todo_create, todo_update, todo_delete
 
 app_name = "todos"
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path("", todo_list),
     path("create/", todo_create),
     path("<id>/", todo_detail), #musí být druhé, jinak by se nenašlo create, create za / by se hledalo jako chybně zadané id
+    path("<id>/update/", todo_update),
+    path("<id>/delete/", todo_delete),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -32,3 +32,21 @@ def todo_create(request):
         "form": form
     }
     return render(request, "todo_app/todo_create.html", context)
+
+
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(request.POST or None, instance=todo)
+    if form.is_valid():
+        form.save()
+        return redirect("/")
+    context = {
+        "form": form
+    }
+    return render(request, "todo_app/todo_update.html", context)
+
+
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return redirect("/")
